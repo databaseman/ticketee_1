f591d03484a661debb923943117cedd1a8e4340d
Minh Nguyen <nguyen_ba_minh@yahoo.com>
2017-05-20 19:14:43 -0700

    Added Devise + sign up feature

--- spec/features/editing_tickets_spec.rb
+++ spec/features/editing_tickets_spec.rb
@@ -2,10 +2,12 @@ require "rails_helper"
 RSpec.feature "Users can edit existing tickets" do
   let(:project) { FactoryGirl.create(:project) }
   let(:ticket) { FactoryGirl.create(:ticket, project: project) }
+
   before do
     visit project_ticket_path(project, ticket)
     click_link "Edit Ticket"
   end
+  
   scenario "with valid attributes" do
     fill_in "Name", with: "Make it really shiny!"
     click_button "Update Ticket"
