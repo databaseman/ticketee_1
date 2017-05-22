82a461d322b5f4f9eac430ab9a49c3b16e099ee6
Minh Nguyen <nguyen_ba_minh@yahoo.com>
2017-05-21 18:25:18 -0700

    Link tickets and users upon ticket creation

--- spec/features/editing_tickets_spec.rb
+++ spec/features/editing_tickets_spec.rb
@@ -1,13 +1,16 @@
 require "rails_helper"
 RSpec.feature "Users can edit existing tickets" do
+  let(:author) { FactoryGirl.create(:user) }
   let(:project) { FactoryGirl.create(:project) }
-  let(:ticket) { FactoryGirl.create(:ticket, project: project) }
+  let(:ticket) do
+    FactoryGirl.create(:ticket, project: project, author: author)
+  end
 
   before do
     visit project_ticket_path(project, ticket)
     click_link "Edit Ticket"
   end
-  
+
   scenario "with valid attributes" do
     fill_in "Name", with: "Make it really shiny!"
     click_button "Update Ticket"
